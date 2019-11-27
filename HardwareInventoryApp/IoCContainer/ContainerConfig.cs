65d09d93aae3ec9b55492f776671245001728622
Artur <amarszalek97@wp.pl>
2019-11-27 01:03:36 +0100

    configured autofac

---
+++ HardwareInventoryApp/IoCContainer/ContainerConfig.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using HardwareInventoryService.ServicesReferences.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareInventoryApp.IoCContainer
+{
+    public static class ContainerConfig
+    {
+        public static IContainer _container;
+
+        public static void Configure()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<LoggerService>().AsImplementedInterfaces().SingleInstance();
+            builder.RegisterType<AuthorizationService>().AsImplementedInterfaces().SingleInstance();
+
+            _container = builder.Build();
+        }
+    }
+}
---
+++ HardwareInventoryApp/ViewModels/LoginPanelViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using Caliburn.Micro;
+using HardwareInventoryApp.IoCContainer;
+using HardwareInventoryService.Models.Models.Authorization;
+using HardwareInventoryService.ServicesReferences.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardwareInventoryApp.ViewModels
+{
+    public class LoginPanelViewModel : Screen
+    {
+        private readonly IAuthorizationService _authorizationService;
+
+        public LoginPanelViewModel(/*IAuthorizationService authorizationService*/)
+        {
+            this._authorizationService = ContainerConfig._container.Resolve<IAuthorizationService>();
+
+            var newSession = new Session();
+            newSession.Username = "testowyUsername";
+
+            var test = this._authorizationService.Authorize(newSession);
+
+            var bo = 1;
+        }
+    }
+}
