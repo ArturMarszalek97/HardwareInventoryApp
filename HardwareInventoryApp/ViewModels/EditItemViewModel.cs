d6429548d7fe19662061b651c86b43680c204519
Artur <amarszalek97@wp.pl>
2019-12-19 02:49:14 +0100

    configured edit view model

--- HardwareInventoryApp/ViewModels/EditItemViewModel.cs
+++ HardwareInventoryApp/ViewModels/EditItemViewModel.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace HardwareInventoryApp.ViewModels
 {
-    public class EditItemViewModel
+    public partial class EditItemViewModel : Window
     {
-
+        public EditItemViewModel()
+        {
+            InitializeComponent();
+        }
     }
 }
